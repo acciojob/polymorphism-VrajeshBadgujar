f907c4184cb207d9f65fa74f604e00f7c6109eb5
Vrajesh <vrajesh2badgujar@gmail.com>
2023-05-15 18:22:14 +0530

    initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,35 +1,21 @@
 package com.driver;
 
 public class Main {
-    public static void main(String[] args) {
-        DeluxePizza dp = new DeluxePizza(true);
-        System.out.println(dp.getPrice());
-        dp.addTakeaway();
-//    dp.addExtraCheese();
-        dp.addExtraToppings();
-        dp.addTakeaway();
-        System.out.println(dp.getBill());
-
-        DeluxePizza dp1 = new DeluxePizza(false);
-        System.out.println(dp1.getPrice());
-        dp1.addTakeaway();
-//    dp1.addExtraCheese();
-        dp1.addExtraToppings();
-//    dp1.addExtraCheese();
-        dp1.addTakeaway();
-        System.out.println(dp1.getBill());
-
-        Pizza p = new Pizza(true);
-        System.out.println(p.getPrice());
-//    p.addExtraCheese();
-        p.addExtraToppings();
-        p.addTakeaway();
-        System.out.println(p.getBill());
-
-        Pizza p1 = new Pizza(false);
-        System.out.println(p1.getPrice());
-//    p1.addExtraCheese();
-        p1.addTakeaway();
-        System.out.println(p1.getBill());
+    public static void main(String args[])
+    {
+        Product p=new Product();
+        int a=p.product(1,2);
+        int b=p.product(1,2,3);
+        double c=p.product(3.45,4.5666);
+        System.out.println(a+" "+b+" "+c);
     }
+    public static class Product
+    {
+        public int product(int x, int y) { return x*y;}
+        public int product(int x, int y, int z) { return x*y*z;}
+        public double product(double x, double y) { return x*y;}
+    }
+
 }
+
+
